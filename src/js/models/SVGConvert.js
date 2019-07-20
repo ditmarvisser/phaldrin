99b53655237500134a8c0624492ff73b9e6adee9
ditmarvisser <ditmarvisser@gmail.com>
2019-07-20 10:51:02 +0200

    Added comments

--- src/js/models/SVGConvert.js
+++ src/js/models/SVGConvert.js
@@ -11,23 +11,31 @@ export const convertSVG = type => {
 			.getElementById("mapSVG")
 			.contentDocument.getElementById("Nodes").children
 	).forEach((element, index) => {
+		// Initiate the node object
 		node = {
+			// The name is either the name, or id, or null
 			name: element.attributes["data-name"]
 				? element.attributes["data-name"].textContent
 				: element.attributes.id
 				? element.attributes.id.textContent
 				: null,
+
+			// The coordinates are in the attributes
 			coordinates: [
 				parseFloat(element.attributes.cx.value),
 				parseFloat(element.attributes.cy.value)
 			],
+
+			// Empty connection array for when we later add the edges
 			connections: []
 		};
 
+		// Construct the string that will be entered in the svg
 		node.svgPath = `<circle class="node" id="node-${index}" ${
 			node.name ? `data-name="${node.name}"` : ""
 		} cx="${node.coordinates[0]}" cy="${node.coordinates[1]}" r="10"/>`;
 
+		// Add the node to the graph
 		graph.addNode(index, node);
 	});
 
@@ -44,12 +52,17 @@ export const convertSVG = type => {
 			.getElementById("mapSVG")
 			.contentDocument.getElementById("Edges").children
 	).forEach((element, index) => {
+		// The d attribute of the edge
 		edgePath = element.attributes.d.nodeValue;
+
+		// Get the starting coordinate by splicing the first instruction from the edgePath
 		edgeStartCoordinate = edgePath
 			.split(/(?=[A-Z])/gi)[0]
 			.split(/M|,/)
 			.splice(1)
 			.map(e => parseFloat(e));
+
+		// Get the end coordinate with getPointAtLength of the total length
 		edgeEndCoordinate = [
 			Math.round(
 				element.getPointAtLength(element.getTotalLength()).x * 10
@@ -59,6 +72,7 @@ export const convertSVG = type => {
 			) / 10
 		];
 
+		// Get the start and end node by comparing the coordinates
 		edgeStartNode = undefined;
 		edgeEndNode = undefined;
 
@@ -84,6 +98,7 @@ export const convertSVG = type => {
 			}
 		}
 
+		// Initiate the edge object
 		edge = {
 			name: element.attributes["data-name"]
 				? element.attributes["data-name"].textContent
@@ -95,25 +110,28 @@ export const convertSVG = type => {
 			edgeEndNode,
 			edgePath
 		};
+
+		// Construct the string that will be entered in the svg
 		edge.svgPath = `<path class="edge" id="edge-${index}" ${
 			edge.name ? `data-name="${edge.name}"` : ""
 		} d="${edge.edgePath}"/>`;
 
+		// Add the edge to the graph
 		graph.addEdge(index, edge);
-		// tempDataRoads.push(roadElement);
 	});
 
 	// Create the text for the database files
 	if (type === "svg") {
+		// Create an array of all the node and edge svgPaths, then join them in a string.
 		let nodesSVGArray = [];
 		let node2;
 		for (node2 in graph.adjacencyList.nodes) {
 			nodesSVGArray.push(graph.adjacencyList.nodes[node2].svgPath);
 		}
-		let roadsSVGArray = [];
+		let edgeSVGArray = [];
 		let edge;
 		for (edge in graph.adjacencyList.edges) {
-			roadsSVGArray.push(graph.adjacencyList.edges[edge].svgPath);
+			edgeSVGArray.push(graph.adjacencyList.edges[edge].svgPath);
 		}
 		return `
 			<?xml-stylesheet type="text/css" href="../css/svg.css" ?>
@@ -127,11 +145,12 @@ export const convertSVG = type => {
 			${nodesSVGArray.join("")}
 			</g>
 			<g id="Edges">
-			${roadsSVGArray.join("")}
+			${edgeSVGArray.join("")}
 			</g>
 			</svg>
 			`;
 	} else if (type === "json") {
+		// Return a stringified version of the graph
 		return JSON.stringify(graph.adjacencyList);
 	}
 };
--- src/js/models/WeightedGraph.js
+++ src/js/models/WeightedGraph.js
@@ -7,11 +7,15 @@ export default class WeightedGraph {
 	}
 
 	addNode(id, node) {
+		// If the node is not yet in the graph, add it
 		if (!this.adjacencyList.nodes[id]) this.adjacencyList.nodes[id] = node;
 	}
-
+	
 	addEdge(id, edge) {
+		// If the edge is not yet in the graph, add it
 		if (!this.adjacencyList.edges[id]) this.adjacencyList.edges[id] = edge;
+
+		// Connect the 2 nodes with each other using the edge
 		this.adjacencyList.nodes[edge.edgeStartNode].connections.push({ node: edge.edgeEndNode, edge: id });
 		this.adjacencyList.nodes[edge.edgeEndNode].connections.push({ node: edge.edgeStartNode, edge: id });
 	}
