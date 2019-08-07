3c8277429edf44c93e2783b43a7fc4a46e37036c
ditmarvisser <ditmarvisser@gmail.com>
2019-08-07 20:41:15 +0200

    Calculate and display the traveled distance/time

--- src/js/views/pathfindView.js
+++ src/js/views/pathfindView.js
@@ -1,3 +1,5 @@
+import data from "../models/data";
+
 export const displayActiveNode = node => {
 	node.path[0].classList.toggle("nodeActive");
 };
@@ -15,9 +17,20 @@ export const displayPath = completedPath => {
 	const svgRoads = document
 		.getElementById("mapSVG")
 		.contentDocument.getElementById("Edges");
+	let traveledDistance = 0;
 	completedPath.forEach(e => {
 		svgRoads.children[e[1]].classList.add("edgeActive");
+		traveledDistance += data.edges[e[1]].edgeWeight;
 	});
+
+	// console.log(data);
+
+	document.querySelector(".traveled-time-distance").innerHTML = `${Math.round(
+		traveledDistance / 6
+	)} miles (${Math.round((traveledDistance / 6) * 1.609)} kilometers)`;
+	document.querySelector(".traveled-time-time").innerHTML = `${Math.round(
+		(traveledDistance / 6 / 24) * 10
+	) / 10} days`;
 };
 
 export const clearDisplayedPath = () => {
