ceb7c8ebdfe47e8949b8bb9b43ac17a6f977e4f1
ditmarvisser <ditmarvisser@gmail.com>
2020-09-27 17:50:26 +0200

    Initiated Typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,43 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: ['@babel/polyfill', './src/js/index.js'],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle.js'
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-			},
+	entry: ["./src/js/index.ts"],
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "js/bundle.js",
+	},
+	devServer: {
+		contentBase: "./dist",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "./src/index.html",
+		}),
+	],
+	module: {
+		rules: [
 			{
 				test: /\.geojson$/,
 				exclude: /node_modules/,
 				use: {
-					loader: 'json-loader'
-				}
-			}
-        ]
-    }
+					loader: "json-loader",
+				},
+			},
+			{
+				test: /\.(t|j)sx?$/,
+				use: {
+					loader: 'awesome-typescript-loader'
+				} 
+			},
+			{
+				enforce: 'pre',
+				test: /\.js$/,
+				use: {
+					loader: 'source-map-loader'
+				},
+			},
+		],
+	},
 };
